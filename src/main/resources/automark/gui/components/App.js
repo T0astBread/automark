c296e023fe4aeb687dd3d9dcd4c55ee9f8fc75b2
T0astBread <tb@t0ast.cc>
2020-07-21 15:17:56 +0200

    GUI: Add curtain

--- src/main/resources/automark/gui/components/App.js
+++ src/main/resources/automark/gui/components/App.js
@@ -6,14 +6,19 @@ export default class App extends Component {
     constructor() {
         super()
         this.state = {
-            workingDir: null,
-            creationWizardIsOpen: false,
+//            workingDir: null,
+//            creationWizardIsOpen: false,
+            a: true
         }
     }
 
     render() {
         return html`
+            <button onClick="${() => this.setState({a: true})}">Down</button>
             <${Dashboard} />
+            <div class="curtain ${this.state.a ? 'up' : ''}">
+                <button onClick="${() => this.setState({a: false})}">Up</button>
+            </div>
         `
     }
 }
